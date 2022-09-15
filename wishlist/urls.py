c7873b67cc19e76a866d8edffb898eb330af112c
Dave Matthew Peter Lumban Tobing <dave.matthew@ui.ac.id>
2022-09-15 11:33:37 +0700

    tutorial 2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist, show_xml, show_json,show_xml_by_id
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_xml, name='show_xml'), #sesuaikan dengan nama fungsi yang dibuat
+    path('json/', show_json, name='show_json'), #sesuaikan dengan nama fungsi yang dibuat
+    path('xml/1', show_xml_by_id, name='show_xml_by_id'), #sesuaikan dengan nama fungsi yang dibuat
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,11 +1,31 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
 # Create your views here.
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def show_xml_by_id(request):
+   data = BarangWishlist.objects.filter(pk=1)
+   return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+
+
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
     'list_barang': data_barang_wishlist,
-    'nama': 'Dave Matthew Peter Lumban Tobing'
-}
+    'nama': 'Dave Matthew Peter Lumban Tobing'}
     return render(request, "wishlist.html", context)
+
+
+
