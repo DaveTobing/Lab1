0b28339e60c84d6484f3ea4e644056405727c456
Dave Matthew Peter Lumban Tobing <dave.matthew@ui.ac.id>
2022-09-21 22:07:44 +0700

    lab3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist, show_xml, show_json,show_xml_by_id
+from wishlist.views import show_wishlist, show_xml, show_json,show_xml_by_id, register, login_user, logout_user
 
 app_name = 'wishlist'
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'), #sesuaikan dengan nama fungsi yang dibuat
     path('json/', show_json, name='show_json'), #sesuaikan dengan nama fungsi yang dibuat
     path('xml/1', show_xml_by_id, name='show_xml_by_id'), #sesuaikan dengan nama fungsi yang dibuat
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
+    path('logout/', logout_user, name='logout'), #sesuaikan dengan nama fungsi yang dibuat
 ]
